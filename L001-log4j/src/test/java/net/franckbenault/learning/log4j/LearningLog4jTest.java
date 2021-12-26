f42b7864281ecafad951530f360d1ae5850f28c8
LAPTOP-UIPBLUVT\franc <franck-benault@orange.fr>
2021-12-26 20:43:09 +0100

    [LOG4J1] complete test update maven version

--- L001-log4j/src/test/java/net/franckbenault/learning/log4j/LearningLog4jTest.java
+++ L001-log4j/src/test/java/net/franckbenault/learning/log4j/LearningLog4jTest.java
@@ -1,27 +1,24 @@
 package net.franckbenault.learning.log4j;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Logger;
-import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 
 class LearningLog4jTest {
 
-
+	private static final Logger LOGGER =  Logger.getLogger(LearningLog4jTest.class);
 
 	@Test
-	void testLogCreate() {
+	public void testLogCreate() {
 		
 		Logger logger = Logger.getLogger("MyLogger");
 		logger.info("Hello1");
-
+		LOGGER.info("Hello1");
 
 	}
 	
 	@Test
-	void testLogAndAppender() {
+	public void testLogAndAppender() {
 		
 		Logger logger = Logger.getLogger("MyLogger");
 		ConsoleAppender appender = new ConsoleAppender();
@@ -30,5 +27,44 @@ class LearningLog4jTest {
 		logger.info("Hello2");
 
 	}
+	
+	@Test
+	public void testLogLevel() {
+		
+		Logger logger = Logger.getLogger("MyLogger");
+		
+		logger.trace("Hello3");
+		LOGGER.trace("Hello3");
+		
+		logger.debug("Hello3");
+		LOGGER.debug("Hello3");
+		
+		logger.info("Hello3");
+		LOGGER.info("Hello3");
+		
+		logger.error("Hello3");
+		LOGGER.error("Hello3");
+		
+		logger.fatal("Hello3");
+		LOGGER.fatal("Hello3");
+		
+	}
+	
+	@Test
+	public void testLogIsDebugEnabled() {
+		
+		Logger logger = Logger.getLogger("MyLogger");
+		
+		if(logger.isDebugEnabled()) logger.trace("Hello3");
+		if(LOGGER.isDebugEnabled()) LOGGER.trace("Hello3");
+		
+		if(logger.isDebugEnabled()) logger.debug("Hello3");
+		if(LOGGER.isDebugEnabled()) LOGGER.debug("Hello3");
+		
+		if(logger.isDebugEnabled()) logger.info("Hello3");
+		if(LOGGER.isDebugEnabled())LOGGER.info("Hello3");
+		
+	}
+
 
 }
