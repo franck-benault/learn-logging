f054b4453444ea3b882cfac9cb6f6290c94873c9
LAPTOP-UIPBLUVT\franc <franck-benault@orange.fr>
2021-12-27 11:19:05 +0100

    [SLF4J2] complete examples

---
+++ L005-slf4j2/src/test/java/net/franckbenault/learning/slf4j2/LearningSlf4j2Test.java
@@ -0,0 +1,94 @@
+package net.franckbenault.learning.slf4j2;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class LearningSlf4j2Test {
+
+	private static final Logger LOGGER =  LoggerFactory.getLogger(LearningSlf4j2Test.class);
+
+	@Test
+	public void testLogCreate() {
+		
+		Logger logger = LoggerFactory.getLogger("MyLogger");
+		logger.info("Hello1");
+		LOGGER.info("Hello1");
+
+	}
+	
+	
+	@Test
+	public void testLogLevel() {
+		
+		Logger logger = LoggerFactory.getLogger("MyLogger");
+		
+		logger.trace("Hello3");
+		LOGGER.trace("Hello3");
+		
+		logger.debug("Hello3");
+		LOGGER.debug("Hello3");
+		
+		logger.info("Hello3");
+		LOGGER.info("Hello3");
+		
+		logger.warn("Hello3");
+		LOGGER.warn("Hello3");
+		
+		logger.error("Hello3");
+		LOGGER.error("Hello3");
+		
+		//logger.fatal("Hello3");
+		//LOGGER.fatal("Hello3");
+		
+	}
+	
+	@Test
+	public void testLogIsXXEnabled() {
+		
+		Logger logger = LoggerFactory.getLogger("MyLogger");
+		
+		if(logger.isTraceEnabled()) logger.trace("Hello3");
+		if(LOGGER.isTraceEnabled()) LOGGER.trace("Hello3");
+		
+		if(logger.isDebugEnabled()) logger.debug("Hello3");
+		if(LOGGER.isDebugEnabled()) LOGGER.debug("Hello3");
+		
+		if(logger.isInfoEnabled()) logger.info("Hello3");
+		if(LOGGER.isInfoEnabled()) LOGGER.info("Hello3");
+		
+		if(logger.isErrorEnabled()) logger.error("Hello3");
+		if(LOGGER.isErrorEnabled()) LOGGER.error("Hello3");
+		
+	}
+	
+	@Test
+	public void testLogFluentApi() {
+		
+		Logger logger = LoggerFactory.getLogger("MyLogger");
+		
+		String simpleMessage = "Hello4";
+		logger.atTrace().log(simpleMessage);
+		LOGGER.atTrace().log(simpleMessage);
+		
+		logger.atDebug().log(simpleMessage);
+		LOGGER.atDebug().log(simpleMessage);
+		
+		logger.atInfo().log(simpleMessage);
+		LOGGER.atInfo().log(simpleMessage);
+		
+		logger.atWarn().log(simpleMessage);
+		LOGGER.atWarn().log(simpleMessage);
+		
+		logger.atError().log(simpleMessage);
+		LOGGER.atError().log(simpleMessage);
+		
+        int newT = 15;
+        int oldT = 16;
+        // using fluent API
+        logger.atInfo().addKeyValue("oldT", oldT).addKeyValue("newT", newT).log("Temperature changed.");          
+		
+	}
+
+}
--- README.md
+++ README.md
@@ -6,3 +6,18 @@ This library is deprecated and mustn't be used anymore.
 But if you have a dependency which needs log4j 1.X.
 You should be to use the brigde library to log4j2.
 
+## Slf4j 1.7.X
+Current stable version
+it has no fluent API
+it has no level FATAL
+
+
+## Slf4j 2.X
+### Fluent API
+Example
+
+logger.atInfo().log(simpleMessage);
+
+### Issues
+need java9 for integration with log4j2
+
